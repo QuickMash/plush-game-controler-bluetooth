ea5a0efc52bbfb213be7eb300ea48d4e69284b01
QuickMash <106212829+QuickMash@users.noreply.github.com>
2022-12-16 16:26:59 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-forever(function () {
-	
+input.onTemperatureConditionChanged(TemperatureCondition.Hot, 80, TemperatureUnit.Fahrenheit, function () {
+    light.setAll(0xff0000)
+})
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+    keyboard.key("Warning! Unit Overheating", KeyboardKeyEvent.Press)
 })
